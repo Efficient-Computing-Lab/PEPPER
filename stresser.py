55313ea966fb8a7b9a1fa1cb002fae4c150f3961
korontanis <gkorod2@gmail.com>
2025-07-17 11:56:28 +0300

    update stresser

--- stresser.py
+++ stresser.py
@@ -2,6 +2,7 @@ import os
 import signal
 import subprocess
 import random
+import psutil
 
 PID_FILE = "stresser_pid.txt"
 
@@ -28,6 +29,22 @@ def end_stresser():
         os.remove(PID_FILE)
     else:
         print("PID file not found. Is stress-ng running?")
+
+def get_metrics(random_cpu_cores,random_cpu_load):
+    print("Calculating metrics...")
+
+    cpu_usage = (random_cpu_cores * random_cpu_load) / 4
+    memory_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    # Create a JSON with the metrics
+    metrics = {
+
+                "total_memory_usage_percent": memory_usage,
+                "total_cpu_usage_percent": cpu_usage,
+                "device_disk_usage_percent": disk_usage,
+                "device_cpu_cores": random_cpu_cores,
+                "device_load_percent": random_cpu_load,
+            }
 
 if __name__ == '__main__':
     random_cpu_load = random.randint(20, 80)

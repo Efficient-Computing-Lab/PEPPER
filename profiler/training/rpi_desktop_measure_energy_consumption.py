7e43228d525ea4b195cdf12ceb6ab610215807ac
korontanis <gkorod2@gmail.com>
2025-09-15 17:33:34 +0300

    update rpi/desktop energy consumption

--- profiler/training/rpi_desktop_measure_energy_consumption.py
+++ profiler/training/rpi_desktop_measure_energy_consumption.py
@@ -2,6 +2,7 @@ import subprocess
 import pandas as pd
 import os
 import time
+import numpy as np
 
 def measure_method():
     """
@@ -17,4 +18,5 @@ def measure_method():
     # Read the CSV file with specified column names
     df = pd.read_csv(os.getcwd() + '/output.csv', header=None, names=columns)
     energy_value = df['CPU Power'].iloc[0]
+    energy_value = np.float64(energy_value)
     return energy_value
